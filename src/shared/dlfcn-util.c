systemd/systemd

dlfcn-util: invert function naming and add helper that does the whole job
We warn when the operation fails, not when it succeeds. Hence this should be
"<do>_or_<handle failure>", not "<do>_and_<handle failure>". We *could* use
whatever convention we want, but rust and perl are rather consistent in using
the logical convention. We don't care about perl that much, but having a naming
convention inverted wrt. rust would be rather confusing.

Also, pretty much every implementation does similar steps, so add a nice
wrapper which combines opening of the library and loading of the symbols.

Also add missing sentinel attribute in dlopen_or_warn().

src/shared/dlfcn-util.c
@@ -2,39 +2,63 @@
 
 #include "dlfcn-util.h"
 
-int dlsym_many_and_warn(void *dl, int level, ...) {
-        va_list ap;
-        int r;
+static int dlsym_many_or_warnv(void *dl, int log_level, va_list ap) {
+        void (**fn)(void);
 
-        /* Tries to resolve a bunch of function symbols, and logs errors about the ones it cannot
-         * resolve. Note that this function possibly modifies the supplied function pointers if the whole
-         * operation fails */
+        /* Tries to resolve a bunch of function symbols, and logs an error about if it cannot resolve one of
+         * them. Note that this function possibly modifies the supplied function pointers if the whole
+         * operation fails. */
 
-        va_start(ap, level);
-
-        for (;;) {
-                void (**fn)(void);
+        while ((fn = va_arg(ap, typeof(fn)))) {
                 void (*tfn)(void);
                 const char *symbol;
-
-                fn = va_arg(ap, typeof(fn));
-                if (!fn)
-                        break;
 
                 symbol = va_arg(ap, typeof(symbol));
 
                 tfn = (typeof(tfn)) dlsym(dl, symbol);
-                if (!tfn) {
-                        r = log_full_errno(level,
-                                           SYNTHETIC_ERRNO(ELIBBAD),
-                                           "Can't find symbol %s: %s", symbol, dlerror());
-                        va_end(ap);
-                        return r;
-                }
-
+                if (!tfn)
+                        return log_full_errno(log_level,
+                                              SYNTHETIC_ERRNO(ELIBBAD),
+                                              "Can't find symbol %s: %s", symbol, dlerror());
                 *fn = tfn;
         }
 
-        va_end(ap);
         return 0;
+}
+
+int dlsym_many_or_warn(void *dl, int log_level, ...) {
+        va_list ap;
+        int r;
+
+        va_start(ap, log_level);
+        r = dlsym_many_or_warnv(dl, log_level, ap);
+        va_end(ap);
+
+        return r;
+}
+
+int dlopen_many_sym_or_warn_sentinel(void **ret_dl, const char *filename, int log_level, ...) {
+        _cleanup_(dlclosep) void *dl = NULL;
+        int r;
+
+        if (*ret_dl)
+                return 0; /* Already loaded */
+
+        dl = dlopen(filename, RTLD_LAZY);
+        if (!dl)
+                return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
+                                       "%s is not installed: %s", filename, dlerror());
+
+        va_list ap;
+        va_start(ap, log_level);
+        r = dlsym_many_or_warnv(dl, log_level, ap);
+        va_end(ap);
+
+        if (r < 0)
+                return r;
+
+        /* Note that we never release the reference here, because there's no real reason to. After all this
+         * was traditionally a regular shared library dependency which lives forever too. */
+        *ret_dl = TAKE_PTR(dl);
+        return 1;
 }

src/shared/dlfcn-util.h
@@ -7,9 +7,13 @@
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(void*, dlclose, NULL);
 
-int dlsym_many_and_warn(void *dl, int level, ...);
+int dlsym_many_or_warn(void *dl, int log_level, ...) _sentinel_;
+int dlopen_many_sym_or_warn_sentinel(void **dl, const char *filename, int log_level, ...) _sentinel_;
 
-/* Macro useful for putting together variable/symbol name pairs when calling dlsym_many_and_warn(). Assumes
+#define dlopen_many_sym_or_warn(dl, filename, log_level, ...) \
+        dlopen_many_sym_or_warn_sentinel(dl, filename, log_level, __VA_ARGS__, NULL)
+
+/* Macro useful for putting together variable/symbol name pairs when calling dlsym_many_or_warn(). Assumes
  * that each library symbol to resolve will be placed in a variable with the "sym_" prefix, i.e. a symbol
  * "foobar" is loaded into a variable "sym_foobar". */
 #define DLSYM_ARG(arg) \

redhat-plumbers/systemd-rhel9

src/shared/dlfcn-util.c
@@ -2,39 +2,63 @@
 
 #include "dlfcn-util.h"
 
-int dlsym_many_and_warn(void *dl, int level, ...) {
-        va_list ap;
-        int r;
+static int dlsym_many_or_warnv(void *dl, int log_level, va_list ap) {
+        void (**fn)(void);
 
-        /* Tries to resolve a bunch of function symbols, and logs errors about the ones it cannot
-         * resolve. Note that this function possibly modifies the supplied function pointers if the whole
-         * operation fails */
+        /* Tries to resolve a bunch of function symbols, and logs an error about if it cannot resolve one of
+         * them. Note that this function possibly modifies the supplied function pointers if the whole
+         * operation fails. */
 
-        va_start(ap, level);
-
-        for (;;) {
-                void (**fn)(void);
+        while ((fn = va_arg(ap, typeof(fn)))) {
                 void (*tfn)(void);
                 const char *symbol;
-
-                fn = va_arg(ap, typeof(fn));
-                if (!fn)
-                        break;
 
                 symbol = va_arg(ap, typeof(symbol));
 
                 tfn = (typeof(tfn)) dlsym(dl, symbol);
-                if (!tfn) {
-                        r = log_full_errno(level,
-                                           SYNTHETIC_ERRNO(ELIBBAD),
-                                           "Can't find symbol %s: %s", symbol, dlerror());
-                        va_end(ap);
-                        return r;
-                }
-
+                if (!tfn)
+                        return log_full_errno(log_level,
+                                              SYNTHETIC_ERRNO(ELIBBAD),
+                                              "Can't find symbol %s: %s", symbol, dlerror());
                 *fn = tfn;
         }
 
-        va_end(ap);
         return 0;
+}
+
+int dlsym_many_or_warn(void *dl, int log_level, ...) {
+        va_list ap;
+        int r;
+
+        va_start(ap, log_level);
+        r = dlsym_many_or_warnv(dl, log_level, ap);
+        va_end(ap);
+
+        return r;
+}
+
+int dlopen_many_sym_or_warn_sentinel(void **dlp, const char *filename, int log_level, ...) {
+        _cleanup_(dlclosep) void *dl = NULL;
+        int r;
+
+        if (*dlp)
+                return 0; /* Already loaded */
+
+        dl = dlopen(filename, RTLD_LAZY);
+        if (!dl)
+                return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
+                                       "%s is not installed: %s", filename, dlerror());
+
+        va_list ap;
+        va_start(ap, log_level);
+        r = dlsym_many_or_warnv(dl, log_level, ap);
+        va_end(ap);
+
+        if (r < 0)
+                return r;
+
+        /* Note that we never release the reference here, because there's no real reason to. After all this
+         * was traditionally a regular shared library dependency which lives forever too. */
+        *dlp = TAKE_PTR(dl);
+        return 1;
 }

src/shared/dlfcn-util.h
@@ -7,9 +7,13 @@
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(void*, dlclose, NULL);
 
-int dlsym_many_and_warn(void *dl, int level, ...);
+int dlsym_many_or_warn(void *dl, int log_level, ...) _sentinel_;
+int dlopen_many_sym_or_warn_sentinel(void **dlp, const char *filename, int log_level, ...) _sentinel_;
 
-/* Macro useful for putting together variable/symbol name pairs when calling dlsym_many_and_warn(). Assumes
+#define dlopen_many_sym_or_warn(dlp, filename, log_level, ...) \
+        dlopen_many_sym_or_warn_sentinel(dlp, filename, log_level, __VA_ARGS__, NULL)
+
+/* Macro useful for putting together variable/symbol name pairs when calling dlsym_many_or_warn(). Assumes
  * that each library symbol to resolve will be placed in a variable with the "sym_" prefix, i.e. a symbol
  * "foobar" is loaded into a variable "sym_foobar". */
 #define DLSYM_ARG(arg) \