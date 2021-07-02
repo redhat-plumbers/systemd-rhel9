systemd/systemd

coredump: parse and append package metadata to journal message
Append 'package' and 'packageVersion' to the journal as discrete fields
COREDUMP_PKGMETA_PACKAGE and COREDUMP_PKGMETA_PACKAGEVERSION respectively,
and the full json blurb as COREDUMP_PKGMETA_JSON.

src/coredump/stacktrace.c
@@ -309,7 +309,7 @@ static int module_callback(Dwfl_Module *mod, void **userdata, const char *name, 
         return DWARF_CB_OK;
 }
 
-static int parse_core(int fd, const char *executable, char **ret) {
+static int parse_core(int fd, const char *executable, char **ret, Hashmap **ret_package_metadata) {
 
         static const Dwfl_Callbacks callbacks = {
                 .find_elf = dwfl_build_id_find_elf,
@@ -381,6 +381,8 @@ static int parse_core(int fd, const char *executable, char **ret) {
         c.f = safe_fclose(c.f);
 
         *ret = TAKE_PTR(buf);
+        if (ret_package_metadata)
+                *ret_package_metadata = TAKE_PTR(package_metadata);
 
         r = 0;
 
@@ -398,10 +400,10 @@ finish:
         return r;
 }
 
-void coredump_parse_core(int fd, const char *executable, char **ret) {
+void coredump_parse_core(int fd, const char *executable, char **ret, Hashmap **ret_package_metadata) {
         int r;
 
-        r = parse_core(fd, executable, ret);
+        r = parse_core(fd, executable, ret, ret_package_metadata);
         if (r == -EINVAL)
                 log_warning("Failed to generate stack trace: %s", dwfl_errmsg(dwfl_errno()));
         else if (r < 0)

src/coredump/stacktrace.h
@@ -4,7 +4,7 @@
 #include "hashmap.h"
 #include "json.h"
 
-void coredump_parse_core(int fd, const char *executable, char **ret);
+void coredump_parse_core(int fd, const char *executable, char **ret, Hashmap **ret_package_metadata);
 
 static inline Hashmap* json_variant_hashmap_free(Hashmap *h) {
         return hashmap_free_with_destructor(h, json_variant_unrefp);

redhat-plumbers/systemd-rhel9

src/coredump/stacktrace.c
@@ -324,7 +324,7 @@ static int module_callback(Dwfl_Module *mod, void **userdata, const char *name, 
         return DWARF_CB_OK;
 }
 
-static int parse_core(int fd, const char *executable, char **ret) {
+static int parse_core(int fd, const char *executable, char **ret, JsonVariant **ret_package_metadata) {
 
         static const Dwfl_Callbacks callbacks = {
                 .find_elf = dwfl_build_id_find_elf,
@@ -394,6 +394,8 @@ static int parse_core(int fd, const char *executable, char **ret) {
         c.f = safe_fclose(c.f);
 
         *ret = TAKE_PTR(buf);
+        if (ret_package_metadata)
+                *ret_package_metadata = TAKE_PTR(package_metadata);
 
         r = 0;
 
@@ -411,10 +413,10 @@ finish:
         return r;
 }
 
-void coredump_parse_core(int fd, const char *executable, char **ret) {
+void coredump_parse_core(int fd, const char *executable, char **ret, JsonVariant **ret_package_metadata) {
         int r;
 
-        r = parse_core(fd, executable, ret);
+        r = parse_core(fd, executable, ret, ret_package_metadata);
         if (r == -EINVAL)
                 log_warning("Failed to generate stack trace: %s", dwfl_errmsg(dwfl_errno()));
         else if (r < 0)

src/coredump/stacktrace.h
@@ -3,4 +3,4 @@
 
 #include "json.h"
 
-void coredump_parse_core(int fd, const char *executable, char **ret);
+void coredump_parse_core(int fd, const char *executable, char **ret, JsonVariant **ret_package_metadata);