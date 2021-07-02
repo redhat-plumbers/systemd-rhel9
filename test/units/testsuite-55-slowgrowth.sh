systemd/systemd

test: use set -eux and set -o pipefail everywhere
This should make the scripts more robust.

test/units/testsuite-55-slowgrowth.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+set -eux
+set -o pipefail
 
 PAGE_SIZE=$(getconf PAGE_SIZE)
 BLOAT_ITERATION_TARGET=$(( 100 << 20 )) # 100 MB

redhat-plumbers/systemd-rhel9

test/units/testsuite-55-slowgrowth.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+# Don't use set -x here, since it generates a lot of output and slows
+# the script down, causing unexpected test fails.
+set -eu
+set -o pipefail
 
 PAGE_SIZE=$(getconf PAGE_SIZE)
 BLOAT_ITERATION_TARGET=$(( 100 << 20 )) # 100 MB