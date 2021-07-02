systemd/systemd

compress: return uncompressed size to the caller
Useful when compressing anonymous FDs that cannot be rewund

src/libsystemd/sd-journal/compress.h
@@ -64,9 +64,9 @@ int decompress_startswith(int compression,
                           const void *prefix, size_t prefix_len,
                           uint8_t extra);
 
-int compress_stream_xz(int fdf, int fdt, uint64_t max_bytes);
-int compress_stream_lz4(int fdf, int fdt, uint64_t max_bytes);
-int compress_stream_zstd(int fdf, int fdt, uint64_t max_bytes);
+int compress_stream_xz(int fdf, int fdt, uint64_t max_bytes, size_t *ret_uncompressed_size);
+int compress_stream_lz4(int fdf, int fdt, uint64_t max_bytes, size_t *ret_uncompressed_size);
+int compress_stream_zstd(int fdf, int fdt, uint64_t max_bytes, size_t *ret_uncompressed_size);
 
 int decompress_stream_xz(int fdf, int fdt, uint64_t max_size);
 int decompress_stream_lz4(int fdf, int fdt, uint64_t max_size);
@@ -82,7 +82,7 @@ int decompress_stream_zstd(int fdf, int fdt, uint64_t max_size);
 #  define compress_stream compress_stream_xz
 #  define COMPRESSED_EXT ".xz"
 #else
-static inline int compress_stream(int fdf, int fdt, uint64_t max_size) {
+static inline int compress_stream(int fdf, int fdt, uint64_t max_size, size_t *ret_uncompressed_size) {
         return -EOPNOTSUPP;
 }
 #  define COMPRESSED_EXT ""

src/libsystemd/sd-journal/test-compress.c
@@ -41,7 +41,7 @@ typedef int (decompress_sw_t)(const void *src, uint64_t src_size,
                               const void *prefix, size_t prefix_len,
                               uint8_t extra);
 
-typedef int (compress_stream_t)(int fdf, int fdt, uint64_t max_bytes);
+typedef int (compress_stream_t)(int fdf, int fdt, uint64_t max_bytes, size_t *uncompressed_size);
 typedef int (decompress_stream_t)(int fdf, int fdt, uint64_t max_size);
 
 #if HAVE_COMPRESSION
@@ -175,6 +175,7 @@ _unused_ static void test_compress_stream(const char *compression,
         int r;
         _cleanup_free_ char *cmd = NULL, *cmd2 = NULL;
         struct stat st = {};
+        size_t uncompressed_size;
 
         r = find_executable(cat, NULL);
         if (r < 0) {
@@ -192,7 +193,7 @@ _unused_ static void test_compress_stream(const char *compression,
 
         assert_se((dst = mkostemp_safe(pattern)) >= 0);
 
-        assert_se(compress(src, dst, -1) == 0);
+        assert_se(compress(src, dst, -1, &uncompressed_size) == 0);
 
         if (cat) {
                 assert_se(asprintf(&cmd, "%s %s | diff %s -", cat, pattern, srcfile) > 0);
@@ -204,6 +205,7 @@ _unused_ static void test_compress_stream(const char *compression,
         assert_se((dst2 = mkostemp_safe(pattern2)) >= 0);
 
         assert_se(stat(srcfile, &st) == 0);
+        assert_se((size_t)st.st_size == uncompressed_size);
 
         assert_se(lseek(dst, 0, SEEK_SET) == 0);
         r = decompress(dst, dst2, st.st_size);

redhat-plumbers/systemd-rhel9

src/libsystemd/sd-journal/compress.h
@@ -64,9 +64,9 @@ int decompress_startswith(int compression,
                           const void *prefix, size_t prefix_len,
                           uint8_t extra);
 
-int compress_stream_xz(int fdf, int fdt, uint64_t max_bytes);
-int compress_stream_lz4(int fdf, int fdt, uint64_t max_bytes);
-int compress_stream_zstd(int fdf, int fdt, uint64_t max_bytes);
+int compress_stream_xz(int fdf, int fdt, uint64_t max_bytes, uint64_t *ret_uncompressed_size);
+int compress_stream_lz4(int fdf, int fdt, uint64_t max_bytes, uint64_t *ret_uncompressed_size);
+int compress_stream_zstd(int fdf, int fdt, uint64_t max_bytes, uint64_t *ret_uncompressed_size);
 
 int decompress_stream_xz(int fdf, int fdt, uint64_t max_size);
 int decompress_stream_lz4(int fdf, int fdt, uint64_t max_size);
@@ -82,7 +82,7 @@ int decompress_stream_zstd(int fdf, int fdt, uint64_t max_size);
 #  define compress_stream compress_stream_xz
 #  define COMPRESSED_EXT ".xz"
 #else
-static inline int compress_stream(int fdf, int fdt, uint64_t max_size) {
+static inline int compress_stream(int fdf, int fdt, uint64_t max_size, uint64_t *ret_uncompressed_size) {
         return -EOPNOTSUPP;
 }
 #  define COMPRESSED_EXT ""

src/libsystemd/sd-journal/test-compress.c
@@ -41,7 +41,7 @@ typedef int (decompress_sw_t)(const void *src, uint64_t src_size,
                               const void *prefix, size_t prefix_len,
                               uint8_t extra);
 
-typedef int (compress_stream_t)(int fdf, int fdt, uint64_t max_bytes);
+typedef int (compress_stream_t)(int fdf, int fdt, uint64_t max_bytes, uint64_t *uncompressed_size);
 typedef int (decompress_stream_t)(int fdf, int fdt, uint64_t max_size);
 
 #if HAVE_COMPRESSION
@@ -176,6 +176,7 @@ _unused_ static void test_compress_stream(const char *compression,
         int r;
         _cleanup_free_ char *cmd = NULL, *cmd2 = NULL;
         struct stat st = {};
+        uint64_t uncompressed_size;
 
         r = find_executable(cat, NULL);
         if (r < 0) {
@@ -193,7 +194,7 @@ _unused_ static void test_compress_stream(const char *compression,
 
         assert_se((dst = mkostemp_safe(pattern)) >= 0);
 
-        assert_se(compress(src, dst, -1) == 0);
+        assert_se(compress(src, dst, -1, &uncompressed_size) == 0);
 
         if (cat) {
                 assert_se(asprintf(&cmd, "%s %s | diff %s -", cat, pattern, srcfile) > 0);
@@ -205,6 +206,7 @@ _unused_ static void test_compress_stream(const char *compression,
         assert_se((dst2 = mkostemp_safe(pattern2)) >= 0);
 
         assert_se(stat(srcfile, &st) == 0);
+        assert_se((uint64_t)st.st_size == uncompressed_size);
 
         assert_se(lseek(dst, 0, SEEK_SET) == 0);
         r = decompress(dst, dst2, st.st_size);