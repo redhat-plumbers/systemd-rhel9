systemd/systemd

udev: ignore one more error in device_get_block_device()

src/udev/udevd.c
@@ -451,6 +451,8 @@ static int device_get_block_device(sd_device *dev, const char **ret) {
                 return log_device_debug_errno(dev, r, "Failed to get devtype: %m");
         if (r >= 0 && streq(val, "partition")) {
                 r = sd_device_get_parent(dev, &dev);
+                if (r == -ENODEV) /* The device may be already removed. */
+                        goto irrelevant;
                 if (r < 0)
                         return log_device_debug_errno(dev, r, "Failed to get parent device: %m");
         }

redhat-plumbers/systemd-rhel9

(cherry picked from commit 20ba5a3fe7c387eae72ca3e0bd1e6eeee3f50f3b)
Related: #2087652

src/udev/udevd.c
@@ -442,6 +442,8 @@ static int device_get_block_device(sd_device *dev, const char **ret) {
                 return log_device_debug_errno(dev, r, "Failed to get devtype: %m");
         if (r >= 0 && streq(val, "partition")) {
                 r = sd_device_get_parent(dev, &dev);
+                if (r == -ENOENT) /* The device may be already removed. */
+                        goto irrelevant;
                 if (r < 0)
                         return log_device_debug_errno(dev, r, "Failed to get parent device: %m");
         }