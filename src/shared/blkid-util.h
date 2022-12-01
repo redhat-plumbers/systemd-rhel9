fef7b794ef7e53a7a2d0be2963a5fdfd9f77e739
Lennart Poettering <lennart@poettering.net>
2022-12-01 15:37:59 +0100

    blkid-util: define enum for blkid_do_safeprobe() return values
    libblkid really should define an enum for this on its own, but it
    currently doesn't and returns literal numeric values. Lets make this
    more readable by adding our own symbolic names via an enum.
    
    (cherry picked from commit 2e3944b872cf57dbccdda14ec66772e8fdd2273b)
    
    Related: RHEL-16182

--- src/shared/blkid-util.h
+++ src/shared/blkid-util.h
@@ -7,4 +7,14 @@
 #  include "macro.h"
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(blkid_probe, blkid_free_probe, NULL);
+
+/* Define symbolic names for blkid_do_safeprobe() return values, since blkid only uses literal numbers. We
+ * prefix these symbolic definitions with underscores, to not invade libblkid's namespace needlessly. */
+enum {
+        _BLKID_SAFEPROBE_FOUND     =  0,
+        _BLKID_SAFEPROBE_NOT_FOUND =  1,
+        _BLKID_SAFEPROBE_AMBIGUOUS = -2,
+        _BLKID_SAFEPROBE_ERROR     = -1,
+};
+
 #endif
