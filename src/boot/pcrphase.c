6b14fa7bcf40ba6dd289fbf7cda835d37d243dc5
Lennart Poettering <lennart@poettering.net>
2022-12-15 18:07:20 +0100

    pcrphase: gracefully exit if TPM2 support is incomplete
    If everything points to the fact that TPM2 should work, but then the
    driver fails to initialize we should handle this gracefully and not
    cause failing services all over the place.
    
    Fixes: #25700
    (cherry picked from commit 0318d54539fe168822447889ac0e858a10c55f74)
    
    Related: RHEL-16182

--- src/boot/pcrphase.c
+++ src/boot/pcrphase.c
@@ -13,6 +13,7 @@
 #include "tpm-pcr.h"
 #include "tpm2-util.h"
 
+static bool arg_graceful = false;
 static char *arg_tpm2_device = NULL;
 static char **arg_banks = NULL;
 
@@ -34,6 +35,7 @@ static int help(int argc, char *argv[], void *userdata) {
                "     --version           Print version\n"
                "     --bank=DIGEST       Select TPM bank (SHA1, SHA256)\n"
                "     --tpm2-device=PATH  Use specified TPM2 device\n"
+               "     --graceful          Exit gracefully if no TPM2 device is found\n"
                "\nSee the %2$s for details.\n",
                program_invocation_short_name,
                link,
@@ -50,6 +52,7 @@ static int parse_argv(int argc, char *argv[]) {
                 ARG_VERSION = 0x100,
                 ARG_BANK,
                 ARG_TPM2_DEVICE,
+                ARG_GRACEFUL,
         };
 
         static const struct option options[] = {
@@ -57,6 +60,7 @@ static int parse_argv(int argc, char *argv[]) {
                 { "version",     no_argument,       NULL, ARG_VERSION     },
                 { "bank",        required_argument, NULL, ARG_BANK        },
                 { "tpm2-device", required_argument, NULL, ARG_TPM2_DEVICE },
+                { "graceful",    no_argument,       NULL, ARG_GRACEFUL    },
                 {}
         };
 
@@ -103,6 +107,10 @@ static int parse_argv(int argc, char *argv[]) {
                         free_and_replace(arg_tpm2_device, device);
                         break;
                 }
+
+                case ARG_GRACEFUL:
+                        arg_graceful = true;
+                        break;
 
                 case '?':
                         return -EINVAL;
@@ -172,6 +180,11 @@ static int run(int argc, char *argv[]) {
          * disallow an empty word to avoid ambiguities. */
         if (isempty(word))
                 return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "String to measure cannot be empty, refusing.");
+
+        if (arg_graceful && tpm2_support() != TPM2_SUPPORT_FULL) {
+                log_notice("No complete TPM2 support detected, exiting gracefully.");
+                return EXIT_SUCCESS;
+        }
 
         length = strlen(word);
 
