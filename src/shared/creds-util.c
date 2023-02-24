cb40b03557820ed08d46ce172a79ae5af9b0c260
William Roberts <william.c.roberts@intel.com>
2023-02-24 14:11:16 -0600

    tpm2: add support for a trusted SRK
    Prevent attackers from spoofing the tpmKey portion of the AuthSession by
    adding a trusted key to the LUKS header metadata. Also, use a persistent
    object rather than a transient object.
    
    This provides the following benifits:
    1. No way to MITM the tpmKey portion of the session, see [1] for
    details.
    
    2. Strengthens the encrypted sessions, note that the bindKey could be
       dropped now.
    
    3. Speed, once it's created we just use it.
    
    4. Owner Auth is needed to call create primary, so using the SRK
       creates a scratch space for normal users.
    
    This is a "first to set" model, in where the first person to set the key
    in the LUKS header wins. Thus, setup should be done in a known good
    state. If an SRK, which is a primary key at a special persistent
    address, is found, it will use whatever is there. If not, it creates an
    SRK. The SRK follows the convetions used through the tpm2-software
    organization code on GitHub [2], however, a split has occured between
    Windows and Linux with respect to SRK templates. The Linux SRK is
    generated with the unique field size set to 0, in Windows, it properly
    sets the size to key size in bytes and the unique data to all 0's of that
    size. Note the proper templates for SRKs is covered in spec [3].
    However, the most important thing, is that both SRKs are passwordless,
    and thus they should be interchangable. If Windows is the first to make
    the SRK, systemd will gladly accept it and vice-versa.
    
    1. Without the bindKey being utilized, an attacker was able to intercept
    this and fake a key, thus being able to decrypt and encrypt traffic as
    needed. Introduction of the bindKey strengthened this, but allows for
    the attacker to brute force AES128CFB using pin guesses. Introduction of
    the salt increases the difficulty of this attack as well as DA attacks
    on the TPM objects itself.
    
    2. https://github.com/tpm2-software
    
    3. https://trustedcomputinggroup.org/wp-content/uploads/TCG-TPM-v2.0-Provisioning-Guidance-Published-v1r1.pdf
    
    Fixes: #20668
    Fixes: #22637
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    (cherry picked from commit acbb504eaf1be51572b1c0d0d490ac478bc41c64)
    
    Related: RHEL-16182

--- src/shared/creds-util.c
+++ src/shared/creds-util.c
@@ -657,7 +657,9 @@ int encrypt_credential_and_warn(
                               &tpm2_blob, &tpm2_blob_size,
                               &tpm2_policy_hash, &tpm2_policy_hash_size,
                               &tpm2_pcr_bank,
-                              &tpm2_primary_alg);
+                              &tpm2_primary_alg,
+                              /* ret_srk_buf= */ NULL,
+                              /* ret_srk_buf_size= */ 0);
                 if (r < 0) {
                         if (sd_id128_equal(with_key, _CRED_AUTO_INITRD))
                                 log_warning("Firmware reported a TPM2 being present and used, but we didn't manage to talk to it. Credential will be refused if SecureBoot is enabled.");
@@ -987,6 +989,10 @@ int decrypt_credential_and_warn(
                                     le32toh(z->size));
                 }
 
+                /*
+                 * TODO: Add the SRK data to the credential structure so it can be plumbed
+                 * through and used to verify the TPM session.
+                 */
                 r = tpm2_unseal(tpm2_device,
                                 le64toh(t->pcr_mask),
                                 le16toh(t->pcr_bank),
@@ -1000,6 +1006,8 @@ int decrypt_credential_and_warn(
                                 le32toh(t->blob_size),
                                 t->policy_hash_and_blob + le32toh(t->blob_size),
                                 le32toh(t->policy_hash_size),
+                                /* srk_buf= */ NULL,
+                                /* srk_buf_size= */ 0,
                                 &tpm2_key,
                                 &tpm2_key_size);
                 if (r < 0)
