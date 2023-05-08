e8064242398505480f7dcd18b1dc2252a93b36f0
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-08 22:38:34 +0200

    test: merge unit file related tests into TEST-23-UNIT-FILE
    Rename TEST-23-TYPE-EXEC to TEST-23-UNIT-FILE and merge it with
    following tests:
      - TEST-37-RUNTIMEDIRECTORYPRESERV
      - TEST-40-EXEC-COMMAND-EX
      - TEST-41-ONESHOT-RESTART
      - TEST-42-EXECSTOPPOST
      - TEST-57-ONSUCCESS-UPHOLD
    
    (cherry picked from commit 539af5c4413efbcdb57713b7ad563440217b28a5)
    
    Related: #2213521

--- test/units/testsuite-23.oneshot-restart.sh
+++ test/units/testsuite-23.oneshot-restart.sh
@@ -3,21 +3,23 @@
 set -eux
 set -o pipefail
 
+# Test oneshot unit restart on failure
+
 # wait this many secs for each test service to succeed in what is being tested
 MAX_SECS=60
 
 systemd-analyze log-level debug
 
 # test one: Restart=on-failure should restart the service
-systemd-run --unit=one -p Type=oneshot -p Restart=on-failure /bin/bash -c "exit 1" \
+systemd-run --unit=oneshot-restart-one -p Type=oneshot -p Restart=on-failure /bin/bash -c "exit 1" \
     && { echo 'unexpected success'; exit 1; }
 
 for ((secs = 0; secs < MAX_SECS; secs++)); do
-  [[ "$(systemctl show one.service -P NRestarts)" -le 0 ]] || break
-  sleep 1
+    [[ "$(systemctl show oneshot-restart-one.service -P NRestarts)" -le 0 ]] || break
+    sleep 1
 done
-if [[ "$(systemctl show one.service -P NRestarts)" -le 0 ]]; then
-  exit 1
+if [[ "$(systemctl show oneshot-restart-one.service -P NRestarts)" -le 0 ]]; then
+    exit 1
 fi
 
 TMP_FILE="/tmp/test-41-oneshot-restart-test"
@@ -26,7 +28,7 @@ TMP_FILE="/tmp/test-41-oneshot-restart-test"
 
 # test two: make sure StartLimitBurst correctly limits the number of restarts
 # and restarts execution of the unit from the first ExecStart=
-systemd-run --unit=two \
+systemd-run --unit=oneshot-restart-two \
             -p StartLimitIntervalSec=120 \
             -p StartLimitBurst=3 \
             -p Type=oneshot \
@@ -50,7 +52,3 @@ if [[ $(cat $TMP_FILE) != "aaa" ]]; then
 fi
 
 systemd-analyze log-level info
-
-echo OK >/testok
-
-exit 0
