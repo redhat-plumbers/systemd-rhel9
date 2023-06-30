310748b7e978f02b30840eff0bca9d367c01ad77
Dan Streetman <ddstreet@ieee.org>
2023-06-30 12:57:23 -0400

    tpm2: allow tpm2_make_encryption_session() without bind key
    Allow providing no bind key, and use ESYS_TR_NONE instead.
    
    (cherry picked from commit 73592a7cca194a375d78e15df9e33c2d40f2ea74)
    
    Related: RHEL-16182

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -2793,6 +2793,7 @@ static int tpm2_make_encryption_session(
         int r;
 
         assert(c);
+        assert(primary);
         assert(ret_session);
 
         log_debug("Starting HMAC encryption session.");
@@ -2808,7 +2809,7 @@ static int tpm2_make_encryption_session(
         rc = sym_Esys_StartAuthSession(
                         c->esys_context,
                         primary->esys_handle,
-                        bind_key->esys_handle,
+                        bind_key ? bind_key->esys_handle : ESYS_TR_NONE,
                         ESYS_TR_NONE,
                         ESYS_TR_NONE,
                         ESYS_TR_NONE,
@@ -3962,7 +3963,7 @@ int tpm2_seal(Tpm2Context *c,
         }
 
         _cleanup_(tpm2_handle_freep) Tpm2Handle *encryption_session = NULL;
-        r = tpm2_make_encryption_session(c, primary_handle, &TPM2_HANDLE_NONE, &encryption_session);
+        r = tpm2_make_encryption_session(c, primary_handle, /* bind_key= */ NULL, &encryption_session);
         if (r < 0)
                 return r;
 
