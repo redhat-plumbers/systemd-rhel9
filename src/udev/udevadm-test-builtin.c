systemd/systemd

udev: use SYNTHETIC_ERRNO() at one more place

src/udev/udevadm-test-builtin.c
@@ -87,8 +87,7 @@ int builtin_main(int argc, char *argv[], void *userdata) {
 
         cmd = udev_builtin_lookup(arg_command);
         if (cmd < 0) {
-                log_error("Unknown command '%s'", arg_command);
-                r = -EINVAL;
+                r = log_error(SYNTHETIC_ERRNO(EINVAL), "Unknown command '%s'", arg_command);
                 goto finish;
         }
 

redhat-plumbers/systemd-rhel9

(cherry picked from commit b3cfe5900108df81fbf547b297d51ac8c7359a9b)
Related: RHEL-5988

src/udev/udevadm-test-builtin.c
@@ -87,8 +87,7 @@ int builtin_main(int argc, char *argv[], void *userdata) {
 
         cmd = udev_builtin_lookup(arg_command);
         if (cmd < 0) {
-                log_error("Unknown command '%s'", arg_command);
-                r = -EINVAL;
+                r = log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Unknown command '%s'", arg_command);
                 goto finish;
         }
 