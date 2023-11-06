9f752e4ab07850a1dbdc5eeb370a8b8a051f0b65
Dan Streetman <ddstreet@ieee.org>
2023-11-06 13:40:11 -0500

    tpm2: add tpm2_sym_alg_*_string() and tpm2_sym_mode_*_string()
    Add functions to convert between alg id and string name for symmetric
    algorithms and symmetric encryption modes.
    
    (cherry picked from commit 2d784782bf700ae26dbeaa24e11ee8faebc29367)
    
    Related: RHEL-16182

--- src/shared/tpm2-util.h
+++ src/shared/tpm2-util.h
@@ -309,6 +309,12 @@ int tpm2_hash_alg_from_string(const char *alg);
 const char *tpm2_asym_alg_to_string(uint16_t alg);
 int tpm2_asym_alg_from_string(const char *alg);
 
+const char *tpm2_sym_alg_to_string(uint16_t alg) _const_;
+int tpm2_sym_alg_from_string(const char *alg) _pure_;
+
+const char *tpm2_sym_mode_to_string(uint16_t mode) _const_;
+int tpm2_sym_mode_from_string(const char *mode) _pure_;
+
 char *tpm2_pcr_mask_to_string(uint32_t mask);
 
 typedef struct {
