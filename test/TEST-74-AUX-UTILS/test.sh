c8b1862ed0f4a36473238f5305de802b423a656f
Lennart Poettering <lennart@poettering.net>
2024-01-10 11:28:23 +0100

    test: add testcase for ssh generator
    (cherry picked from commit 52d863defcd05d0b676ce743b9eaf1060131f9a1)
    
    Related: RHEL-94900

--- test/TEST-74-AUX-UTILS/test.sh
+++ test/TEST-74-AUX-UTILS/test.sh
@@ -7,4 +7,29 @@ TEST_DESCRIPTION="Tests for auxiliary utilities"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+# Make sure vsock is available in the VM
+CID=$((RANDOM + 3))
+QEMU_OPTIONS+=" -device vhost-vsock-pci,guest-cid=$CID"
+
+test_create_image() {
+    create_empty_image_rootdir
+
+    LOG_LEVEL=5
+
+    setup_basic_environment
+    mask_supporting_services
+
+    generate_module_dependencies
+
+    inst_binary socat
+    inst_binary ssh
+    inst_binary sshd
+    inst_binary ssh-keygen
+    inst_binary usermod
+    instmods vmw_vsock_virtio_transport
+    instmods vsock_loopback
+    instmods vmw_vsock_vmci_transport
+    generate_module_dependencies
+}
+
 do_test "$@"
