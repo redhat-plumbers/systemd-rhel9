611808fe751a85fcbd5b091a109bbc687961e371
Frantisek Sumsal <frantisek@sumsal.cz>
2024-03-05 20:22:19 +0100

    mkosi: use pesign for signing UKI addons
    In C9S/RHEL9 we don't ship sbsign, so we have to use pesign instead.
    Unfortunately, ukify doesn't support singing with pesign, so we have to
    do that manually.
    
    Related: RHEL-27512
    RHEL-only

--- mkosi.images/system/mkosi.extra/usr/lib/systemd/mkosi-check-and-shutdown.sh
+++ mkosi.images/system/mkosi.extra/usr/lib/systemd/mkosi-check-and-shutdown.sh
@@ -10,7 +10,7 @@ if ! systemd-detect-virt --container; then
     cmp /sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c <(printf '\6\0\0\0\1')
     cmp /sys/firmware/efi/efivars/SetupMode-8be4df61-93ca-11d2-aa0d-00e098032b8c <(printf '\6\0\0\0\0')
 
-    if command -v sbsign &>/dev/null; then
+    if command -v pesign &>/dev/null; then
         cat /proc/cmdline
         grep -q this_should_be_here /proc/cmdline
         (! grep -q this_should_not_be_here /proc/cmdline)
