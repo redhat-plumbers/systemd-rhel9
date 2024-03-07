a54a231cff67ddb363e0577b3f960724e5271cc4
Sam Leonard <sam.leonard@codethink.co.uk>
2024-03-07 14:43:28 +0000

    ssh-generator: support ssh.ephemeral-key.all-users
    (cherry picked from commit 071155a1fbac566930c6e062b509da2d65ecbaf5)
    
    Related: RHEL-94900

--- src/ssh-generator/ssh-generator.c
+++ src/ssh-generator/ssh-generator.c
@@ -110,8 +110,9 @@ static int make_sshd_template_unit(
                         "Documentation=man:systemd-ssh-generator(8) man:sshd(8)\n"
                         "\n"
                         "[Service]\n"
-                        "ExecStart=-%s -i\n"
-                        "StandardInput=socket\n",
+                        "ExecStart=-%s -i -o \"AuthorizedKeysFile ${CREDENTIALS_DIRECTORY}/ssh.ephemeral-authorized_keys-all .ssh/authorized_keys\"\n"
+                        "StandardInput=socket\n"
+                        "ImportCredential=ssh.ephemeral-authorized_keys-all",
                         sshd_binary);
 
                 r = fflush_and_check(f);
