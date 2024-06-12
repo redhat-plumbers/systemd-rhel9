systemd/systemd

test: Skip various tests when /sys is not mounted
When running tests in a container, /sys might not be mounted, so
let's make sure we skip tests that depend on /sys in this case.

src/libsystemd/sd-login/test-login.c
@@ -10,6 +10,7 @@
 #include "format-util.h"
 #include "log.h"
 #include "missing_syscall.h"
+#include "mountpoint-util.h"
 #include "process-util.h"
 #include "string-util.h"
 #include "strv.h"
@@ -327,6 +328,9 @@ TEST(monitor) {
 }
 
 static int intro(void) {
+        if (IN_SET(cg_unified(), -ENOENT, -ENOMEDIUM))
+                return log_tests_skipped("cgroups is not mounted");
+
         log_info("/* Information printed is from the live system */");
         return EXIT_SUCCESS;
 }

redhat-plumbers/systemd-rhel9

(cherry picked from commit a412a1b92ab234a57c646f6779471772b2c355ec)
Related: RHEL-27512

src/libsystemd/sd-login/test-login.c
@@ -9,6 +9,7 @@
 #include "fd-util.h"
 #include "format-util.h"
 #include "log.h"
+#include "mountpoint-util.h"
 #include "string-util.h"
 #include "strv.h"
 #include "tests.h"
@@ -295,6 +296,9 @@ TEST(monitor) {
 }
 
 static int intro(void) {
+        if (IN_SET(cg_unified(), -ENOENT, -ENOMEDIUM))
+                return log_tests_skipped("cgroupfs is not mounted");
+
         log_info("/* Information printed is from the live system */");
         return EXIT_SUCCESS;
 }