systemd/systemd

core: log detected confidential virtualization type
Related: https://github.com/systemd/systemd/issues/27604
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/core/main.c
@@ -38,6 +38,7 @@
 #include "crash-handler.h"
 #include "dbus-manager.h"
 #include "dbus.h"
+#include "conf-virt.h"
 #include "constants.h"
 #include "dev-setup.h"
 #include "efi-random.h"
@@ -2119,6 +2120,10 @@ static void log_execution_mode(bool *ret_first_boot) {
                 v = detect_virtualization();
                 if (v > 0)
                         log_info("Detected virtualization %s.", virtualization_to_string(v));
+
+                v = detect_confidential_virtualization();
+                if (v > 0)
+                        log_info("Detected confidential virtualization %s.", confidential_virtualization_to_string(v));
 
                 log_info("Detected architecture %s.", architecture_to_string(uname_architecture()));
 

redhat-plumbers/systemd-rhel9

(cherry picked from commit 024469ddb99ebbf0e0b0f1d77f763116ca251c5d)
Related: RHEL-50651

src/core/main.c
@@ -32,6 +32,7 @@
 #include "cgroup-util.h"
 #include "clock-util.h"
 #include "conf-parser.h"
+#include "confidential-virt.h"
 #include "cpu-set-util.h"
 #include "crash-handler.h"
 #include "dbus-manager.h"
@@ -2059,6 +2060,10 @@ static void log_execution_mode(bool *ret_first_boot) {
                 v = detect_virtualization();
                 if (v > 0)
                         log_info("Detected virtualization %s.", virtualization_to_string(v));
+
+                v = detect_confidential_virtualization();
+                if (v > 0)
+                        log_info("Detected confidential virtualization %s.", confidential_virtualization_to_string(v));
 
                 log_info("Detected architecture %s.", architecture_to_string(uname_architecture()));
 