0dcc470a8c99d5ee6386a16d5fdae50349e04fd3
Lennart Poettering <lennart@poettering.net>
2025-06-12 11:38:21 +0200

    ssh-generator: generate /etc/issue.d/ with VSOCK ssh info data
    I find myself trying to log into a fresh ParticleOS VM started via
    systemd-vmspawn all the time, but I don't know its CID. Let's show it on
    the getty screen, to make it immediately visible.
    
    (cherry picked from commit cfd89202435751a0c46a46a81a6d83fb92f4b4db)
    
    Related: RHEL-94900

--- src/ssh-generator/ssh-generator.c
+++ src/ssh-generator/ssh-generator.c
@@ -130,6 +130,7 @@ static int write_socket_unit(
                 const char *unit,
                 const char *listen_stream,
                 const char *comment,
+                const char *extra,
                 bool with_ssh_access_target_dependency) {
 
         int r;
@@ -165,6 +166,9 @@ static int write_socket_unit(
                 "ListenStream=%s\n"
                 "Accept=yes\n",
                 listen_stream);
+
+        if (extra)
+                fputs(extra, f);
 
         r = fflush_and_check(f);
         if (r < 0)
@@ -239,6 +243,8 @@ static int add_vsock_socket(
                         "sshd-vsock.socket",
                         "vsock::22",
                         "AF_VSOCK",
+                        "ExecStartPost=-/usr/lib/systemd/systemd-ssh-issue --make-vsock\n"
+                        "ExecStopPre=-/usr/lib/systemd/systemd-ssh-issue --rm-vsock\n",
                         /* with_ssh_access_target_dependency= */ true);
         if (r < 0)
                 return r;
@@ -274,6 +280,7 @@ static int add_local_unix_socket(
                         "sshd-unix-local.socket",
                         "/run/ssh-unix-local/socket",
                         "AF_UNIX Local",
+                        /* extra= */ NULL,
                         /* with_ssh_access_target_dependency= */ false);
         if (r < 0)
                 return r;
@@ -330,6 +337,7 @@ static int add_export_unix_socket(
                         "sshd-unix-export.socket",
                         "/run/host/unix-export/ssh",
                         "AF_UNIX Export",
+                        /* extra= */ NULL,
                         /* with_ssh_access_target_dependency= */ true);
         if (r < 0)
                 return r;
@@ -381,6 +389,7 @@ static int add_extra_sockets(
                                 socket ?: "sshd-extra.socket",
                                 *i,
                                 *i,
+                                /* extra= */ NULL,
                                 /* with_ssh_access_target_dependency= */ true);
                 if (r < 0)
                         return r;
