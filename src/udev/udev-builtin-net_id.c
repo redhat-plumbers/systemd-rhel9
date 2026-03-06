bd22c129418a2b086a2e1039ae693d987cce83bb
Luca Boccassi <luca.boccassi@gmail.com>
2026-03-06 19:32:35 +0000

    udev: check for invalid chars in various fields received from the kernel
    (cherry picked from commit 16325b35fa6ecb25f66534a562583ce3b96d52f3)
    
    Resolves: RHEL-163875

--- src/udev/udev-builtin-net_id.c
+++ src/udev/udev-builtin-net_id.c
@@ -39,6 +39,7 @@
 #include "strv.h"
 #include "strxcpyx.h"
 #include "udev-builtin.h"
+#include "utf8.h"
 
 #define ONBOARD_14BIT_INDEX_MAX ((1U << 14) - 1)
 #define ONBOARD_16BIT_INDEX_MAX ((1U << 16) - 1)
@@ -1132,9 +1133,13 @@ static int get_link_info(sd_device *dev, LinkInfo *info) {
                 return r;
 
         r = device_get_sysattr_value_filtered(dev, "phys_port_name", &info->phys_port_name);
-        if (r >= 0)
+        if (r >= 0) {
+                if (!utf8_is_valid(info->phys_port_name) || string_has_cc(info->phys_port_name, /* ok= */ NULL))
+                        return log_device_debug_errno(dev, SYNTHETIC_ERRNO(EINVAL), "Invalid phys_port_name");
+
                 /* Check if phys_port_name indicates virtual device representor */
                 (void) sscanf(info->phys_port_name, "pf%*uvf%d", &info->vf_representor_id);
+        }
 
         r = device_get_sysattr_value_filtered(dev, "address", &s);
         if (r < 0 && r != -ENOENT)
