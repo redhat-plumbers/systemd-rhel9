systemd/systemd

ssh-generator: handle gracefully if AF_VSOCK works, but /dev/vsock doesn't
Apparently this case exist, let's handle it gracefully.

Prompted by: https://github.com/systemd/systemd/pull/31544#issuecomment-1971241397

src/ssh-generator/ssh-generator.c
@@ -207,8 +207,14 @@ static int add_vsock_socket(
         /* Determine the local CID so that we can log it to help users to connect to this VM */
         unsigned local_cid;
         r = vsock_get_local_cid(&local_cid);
-        if (r < 0)
+        if (r < 0) {
+                if (ERRNO_IS_DEVICE_ABSENT(r)) {
+                        log_debug("Not creating AF_VSOCK ssh listener, since /dev/vsock is not available (even though AV_VSOCK is).");
+                        return 0;
+                }
+
                 return log_error_errno(r, "Failed to query local AF_VSOCK CID: %m");
+        }
 
         r = make_sshd_template_unit(
                         dest,

redhat-plumbers/systemd-rhel9

Apparently this case exists, let's handle it gracefully.
(cherry picked from commit f557129ecba0826f2a2bfe055a21e7e33ccb57b0)
Related: RHEL-94900

src/ssh-generator/ssh-generator.c
@@ -205,8 +205,14 @@ static int add_vsock_socket(
         /* Determine the local CID so that we can log it to help users to connect to this VM */
         unsigned local_cid;
         r = vsock_get_local_cid(&local_cid);
-        if (r < 0)
+        if (r < 0) {
+                if (ERRNO_IS_DEVICE_ABSENT(r)) {
+                        log_debug("Not creating AF_VSOCK ssh listener, since /dev/vsock is not available (even though AF_VSOCK is).");
+                        return 0;
+                }
+
                 return log_error_errno(r, "Failed to query local AF_VSOCK CID: %m");
+        }
 
         r = make_sshd_template_unit(
                         dest,